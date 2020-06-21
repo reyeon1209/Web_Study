2c81b7057a9274189060a7386c4443549d6ea8eb
reyeon1209 <reyeon5368@naver.com>
2020-06-22 02:46:34 +0900

    오타 수정

--- 200610/js/example21.js
+++ 200610/js/example21.js
@@ -1,9 +1,11 @@
 function nicknameFunc() {
     if (document.getElementById('yesNick').checked) {
         document.getElementById('nick').style.display = "inline";
-        document.getElementById('nickname').setAttribute('required', true);
-    } else {
-        document.getElementById('nickname').removeAttribute('required');
+        document.getElementById('nickname').setAttribute('required', true); // nickname 필수 입력하게
+    }
+    
+    else {
+        document.getElementById('nickname').removeAttribute('required');    // nickname 필수 입력 X
         document.getElementById('nick').style.display = "none";
     }
 }
